3eb3381bfaad19087aed5add5a0098dcf8a1f477
faresbat99 <faresbat99@gmail.com>
2021-09-09 06:38:49 +0200

    css cards

--- src/App.js
+++ src/App.js
@@ -2,8 +2,46 @@ import "./Custom.css";
 
 function App() {
   return (
-    <div className="outerdiv">
-      <div className="innerdiv">this is green box</div>
+    <div className="App">
+      <div className="card">
+        <img
+          src="https://www.w3schools.com/howto/img_avatar.png"
+          alt="Avatar"
+          style={{ width: "100%" }}
+        />
+        <div className="container">
+          <h4>
+            <b>John Doe</b>
+          </h4>
+          <p>Architect &amp; Engineer</p>
+        </div>
+      </div>
+      <div className="card">
+        <img
+          src="https://www.w3schools.com/howto/img_avatar.png"
+          alt="Avatar"
+          style={{ width: "100%" }}
+        />
+        <div className="container">
+          <h4>
+            <b>John Doe</b>
+          </h4>
+          <p>Architect &amp; Engineer</p>
+        </div>
+      </div>
+      <div className="card">
+        <img
+          src="https://www.w3schools.com/howto/img_avatar.png"
+          alt="Avatar"
+          style={{ width: "100%" }}
+        />
+        <div className="container">
+          <h4>
+            <b>John Doe</b>
+          </h4>
+          <p>Architect &amp; Engineer</p>
+        </div>
+      </div>
     </div>
   );
 }
